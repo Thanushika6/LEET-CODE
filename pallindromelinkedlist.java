07e40e36ee0188b459fb46fcf1224f90f265e3ae
Thanushika <thanushika6305@gmail.com>
2025-06-18 09:21:42 +0530

    Create pallindromelinkedlist.java

---
+++ pallindromelinkedlist.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode secondHalfHead = reverseList(slow);
+        ListNode p1 = head;
+        ListNode p2 = secondHalfHead;
+        while (p2 != null) {
+            if (p1.val != p2.val) return false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return true;
+    }
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = prev;
+            prev = head;
+            head = nextNode;
+        }
+        return prev;
+    }
+}
